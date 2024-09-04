2b80923294dfd53dbd9c3f74b63aa1631508b86e
Tristan Maidment <email@theedman.com>
2024-09-04 12:34:49 -0400

    baseline

---
+++ main.py
@@ -0,0 +1,148 @@
+import argparse
+import os
+import numpy as np
+from pettingzoo.classic import rps_v2, leduc_holdem_v4
+from pettingzoo.atari import boxing_v2
+import random
+
+import ray
+from ray import air, tune
+from ray.air.integrations.wandb import WandbLoggerCallback
+from ray.rllib.algorithms.callbacks import DefaultCallbacks
+from ray.rllib.algorithms.algorithm_config import AlgorithmConfig
+from ray.rllib.algorithms.ppo import (
+    PPO,
+    PPOConfig,
+    PPOTF1Policy,
+    PPOTF2Policy,
+    PPOTorchPolicy,
+)
+from ray.rllib.env import PettingZooEnv
+from ray.rllib.policy.policy import PolicySpec
+from ray.rllib.utils.framework import try_import_tf, try_import_torch
+from ray.rllib.utils.test_utils import check_learning_achieved
+from ray.tune.registry import register_env, register_trainable
+
+from mmd import MMDAPPO
+
+tf1, tf, tfv = try_import_tf()
+torch, _ = try_import_torch()
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--framework",
+    choices=["tf", "tf2", "torch"],
+    default="torch",
+    help="The DL framework specifier.",
+)
+parser.add_argument(
+    "--as-test",
+    action="store_true",
+    help="Whether this script should be run as a test: --stop-reward must "
+    "be achieved within --stop-timesteps AND --stop-iters.",
+)
+parser.add_argument(
+    "--stop-iters", type=int, default=150, help="Number of iterations to train."
+)
+parser.add_argument(
+    "--stop-timesteps", type=int, default=100000, help="Number of timesteps to train."
+)
+parser.add_argument(
+    "--stop-reward",
+    type=float,
+    default=1000.0,
+    help="Reward at which we stop training.",
+)
+
+def env_creator(args):
+    env = rps_v2.env(num_actions=5, max_cycles=15)
+    return env
+
+register_trainable("MMDAPPO", MMDAPPO)
+register_env("NashEnv", lambda config: PettingZooEnv(env_creator(config)))
+
+from ray.rllib.algorithms.callbacks import DefaultCallbacks
+from collections import defaultdict
+
+class ActionDistributionCallback(DefaultCallbacks):
+    def __init__(self):
+        super().__init__()
+
+    def on_postprocess_trajectory(self, *, worker, episode, agent_id, policy_id, policies, postprocessed_batch, original_batches, **kwargs):
+        # Get the actions taken in the current batch
+        actions = postprocessed_batch['actions']
+
+        # Track the count of each action within the episode
+        action_counts = defaultdict(int)
+        for action in actions:
+            action_counts[action] += 1
+
+        # Log the action counts as custom metrics
+        for action, count in action_counts.items():
+            # Record each action's count as a custom metric for the episode
+            episode.custom_metrics[f"action_{action}_count"] = count
+
+    def on_episode_end(self, *, worker, base_env, policies, episode, **kwargs):
+        # Optionally, you could further process or log the data at the end of the episode
+        pass
+
+
+def run_same_policy(args, stop):
+    """Use the same policy for both agents (trivial case)."""
+    config = (
+        PPOConfig()
+        .environment("NashEnv")
+        .framework(args.framework)
+        .callbacks(ActionDistributionCallback)
+        .env_runners(
+            rollout_fragment_length=256,
+            num_env_runners=8
+        )
+        .learners(
+            num_gpus_per_learner=1
+        )
+        # .training(
+        #     train_batch_size='128',
+        # )
+        # .evaluation(
+        #     evaluation_interval=1,
+        #     evaluation_num_workers=1,
+        #     evaluation_duration='auto',
+        #     evaluation_duration_unit='timesteps',
+        #     evaluation_parallel_to_training=True,
+        # )
+        .reporting(
+            min_time_s_per_iteration=30,
+        )
+    )
+    wandb_logger = WandbLoggerCallback(
+        project="rock-paper-scissors",
+        entity="tmaidment",
+        group=None,
+        job_type="training",
+        config=config,
+    )
+
+    results = tune.Tuner(
+        "MMDAPPO",
+        param_space=config,
+        run_config=air.RunConfig(stop=stop, callbacks=[wandb_logger], verbose=1)
+    ).fit()
+
+    if args.as_test:
+        # Check vs 0.0 as we are playing a zero-sum game.
+        check_learning_achieved(results, 0.0)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    ray.init()
+
+    # stop = {
+    #     "training_iteration": args.stop_iters,
+    #     "timesteps_total": args.stop_timesteps,
+    #     "episode_reward_mean": args.stop_reward,
+    # }
+
+    run_same_policy(args, stop=None)
---
+++ mmd.py
@@ -0,0 +1,295 @@
+import gymnasium as gym
+import torch
+import numpy as np
+import logging
+from typing import Any, Dict, List, Optional, Type, Union
+from ray.rllib.algorithms.ppo.ppo_torch_policy import PPOTorchPolicy
+from ray.rllib.algorithms.appo.appo_torch_policy import APPOTorchPolicy
+from ray.rllib.utils.torch_utils import explained_variance, sequence_mask
+from ray.rllib.policy.sample_batch import SampleBatch
+from ray.rllib.evaluation.postprocessing import Postprocessing
+from ray.rllib.utils.numpy import convert_to_numpy
+from typing import Dict, Union, Type
+from ray.rllib.models.modelv2 import ModelV2
+from ray.rllib.models.action_dist import ActionDistribution
+from ray.rllib.algorithms.ppo import PPO
+from ray.rllib.algorithms.appo import APPO
+from ray.rllib.utils.torch_utils import apply_grad_clipping, explained_variance
+from ray.rllib.models.torch.torch_action_dist import TorchDistributionWrapper
+import ray.rllib.algorithms.impala.vtrace_torch as vtrace
+from ray.rllib.utils.typing import TensorType
+from ray.rllib.utils.annotations import override
+import math
+from ray.rllib.utils.torch_utils import (
+    apply_grad_clipping,
+    explained_variance,
+    global_norm,
+    sequence_mask,
+)
+from ray.rllib.models.torch.torch_action_dist import (
+    TorchDistributionWrapper,
+    TorchCategorical,
+)
+from ray.rllib.algorithms.impala.impala_torch_policy import (
+    make_time_major,
+    VTraceOptimizer,
+)
+from ray.rllib.utils.framework import try_import_torch
+
+torch, nn = try_import_torch()
+
+logger = logging.getLogger(__name__)
+
+SMALL_POSITIVE = 1e-10
+DEFAULT_VALUE = 1
+WARMUP_ITERATIONS = 10
+
+class MMDAPPOTorchPolicy(APPOTorchPolicy):
+    def __init__(self, observation_space, action_space, config):
+        # MMD-specific attributes
+        self.temp = config.get("temperature_schedule", lambda t: 1.0)
+        self.mag_lr = config.get("magnet_learning_rate_schedule", lambda t: 0.01)
+        self.objective = config.get("objective", "nash")
+        self._comparator = None
+        self.magnet_policy = None
+        self.iteration = 0
+
+        super().__init__(observation_space, action_space, config)
+
+    @override(APPOTorchPolicy)
+    def make_model(self) -> ModelV2:
+        self.model = super().make_model()
+        self.magnet_policy = super().make_model()
+        return self.model
+
+    def update_magnet_policy(self):
+        mag_lr = self.mag_lr(self.iteration)
+        for param, mag_param in zip(self.model.parameters(), self.magnet_policy.parameters()):
+            mag_param.data = mag_param.data.pow(1 - mag_lr) * param.data.pow(mag_lr)
+
+    def loss(
+        self,
+        model: ModelV2,
+        dist_class: Type[ActionDistribution],
+        train_batch: SampleBatch,
+    ) -> Union[TensorType, List[TensorType]]:
+        """Constructs the loss for APPO.
+
+        With IS modifications and V-trace for Advantage Estimation.
+
+        Args:
+            model (ModelV2): The Model to calculate the loss for.
+            dist_class (Type[ActionDistribution]): The action distr. class.
+            train_batch: The training data.
+
+        Returns:
+            Union[TensorType, List[TensorType]]: A single loss tensor or a list
+                of loss tensors.
+        """
+        target_model = self.target_models[model]
+
+        model_out, _ = model(train_batch)
+        action_dist = dist_class(model_out, model)
+
+        if isinstance(self.action_space, gym.spaces.Discrete):
+            is_multidiscrete = False
+            output_hidden_shape = [self.action_space.n]
+        elif isinstance(self.action_space, gym.spaces.multi_discrete.MultiDiscrete):
+            is_multidiscrete = True
+            output_hidden_shape = self.action_space.nvec.astype(np.int32)
+        else:
+            is_multidiscrete = False
+            output_hidden_shape = 1
+
+        def _make_time_major(*args, **kwargs):
+            return make_time_major(
+                self, train_batch.get(SampleBatch.SEQ_LENS), *args, **kwargs
+            )
+
+        actions = train_batch[SampleBatch.ACTIONS]
+        dones = train_batch[SampleBatch.TERMINATEDS]
+        rewards = train_batch[SampleBatch.REWARDS]
+        behaviour_logits = train_batch[SampleBatch.ACTION_DIST_INPUTS]
+
+        target_model_out, _ = target_model(train_batch)
+
+        prev_action_dist = dist_class(behaviour_logits, model)
+        values = model.value_function()
+        values_time_major = _make_time_major(values)
+        bootstrap_values_time_major = _make_time_major(
+            train_batch[SampleBatch.VALUES_BOOTSTRAPPED]
+        )
+        bootstrap_value = bootstrap_values_time_major[-1]
+
+        if self.is_recurrent():
+            max_seq_len = torch.max(train_batch[SampleBatch.SEQ_LENS])
+            mask = sequence_mask(
+                train_batch[SampleBatch.SEQ_LENS], max_seq_len
+            )
+            mask = torch.reshape(mask, [-1])
+            mask = _make_time_major(mask)
+            num_valid = torch.sum(mask)
+
+            def reduce_mean_valid(t):
+                return torch.sum(t[mask]) / num_valid
+
+        else:
+            reduce_mean_valid = torch.mean
+
+        if self.config["vtrace"]:
+            logger.debug("Using V-Trace surrogate loss (vtrace=True)")
+
+            old_policy_behaviour_logits = target_model_out.detach()
+            old_policy_action_dist = dist_class(old_policy_behaviour_logits, model)
+
+            if isinstance(output_hidden_shape, (list, tuple, np.ndarray)):
+                unpacked_behaviour_logits = torch.split(
+                    behaviour_logits, list(output_hidden_shape), dim=1
+                )
+                unpacked_old_policy_behaviour_logits = torch.split(
+                    old_policy_behaviour_logits, list(output_hidden_shape), dim=1
+                )
+            else:
+                unpacked_behaviour_logits = torch.chunk(
+                    behaviour_logits, output_hidden_shape, dim=1
+                )
+                unpacked_old_policy_behaviour_logits = torch.chunk(
+                    old_policy_behaviour_logits, output_hidden_shape, dim=1
+                )
+
+            # Prepare actions for loss.
+            loss_actions = actions if is_multidiscrete else torch.unsqueeze(actions, dim=1)
+
+            # Prepare KL for loss.
+            _make_time_major(old_policy_action_dist.kl(action_dist))
+            # action_kl = _make_time_major(old_policy_action_dist.kl(action_dist))
+            reverse_action_kl = _make_time_major(action_dist.kl(old_policy_action_dist))
+
+            # Compute vtrace on the CPU for better perf.
+            vtrace_returns = vtrace.multi_from_logits(
+                behaviour_policy_logits=_make_time_major(unpacked_behaviour_logits),
+                target_policy_logits=_make_time_major(unpacked_old_policy_behaviour_logits),
+                actions=torch.unbind(_make_time_major(loss_actions), dim=2),
+                discounts=(1.0 - _make_time_major(dones).float()) * self.config["gamma"],
+                rewards=_make_time_major(rewards),
+                values=values_time_major,
+                bootstrap_value=bootstrap_value,
+                dist_class=TorchCategorical if is_multidiscrete else dist_class,
+                model=model,
+                clip_rho_threshold=self.config["vtrace_clip_rho_threshold"],
+                clip_pg_rho_threshold=self.config["vtrace_clip_pg_rho_threshold"],
+            )
+
+            actions_logp = _make_time_major(action_dist.logp(actions))
+            prev_actions_logp = _make_time_major(prev_action_dist.logp(actions))
+            old_policy_actions_logp = _make_time_major(old_policy_action_dist.logp(actions))
+            is_ratio = torch.clamp(torch.exp(prev_actions_logp - old_policy_actions_logp), 0.0, 2.0)
+            logp_ratio = is_ratio * torch.exp(actions_logp - prev_actions_logp)
+            self._is_ratio = is_ratio
+
+            advantages = vtrace_returns.pg_advantages.to(logp_ratio.device)
+            surrogate_loss = torch.min(
+                advantages * logp_ratio,
+                advantages
+                * torch.clamp(
+                    logp_ratio,
+                    1 - self.config["clip_param"],
+                    1 + self.config["clip_param"],
+                ),
+            )
+
+            # mean_kl_loss = reduce_mean_valid(action_kl)
+            mean_kl_loss = mean_reverse_kl_loss = reduce_mean_valid(reverse_action_kl)
+            mean_policy_loss = -reduce_mean_valid(surrogate_loss)
+
+            # The value function loss.
+            value_targets = vtrace_returns.vs.to(values_time_major.device)
+            delta = values_time_major - value_targets
+            mean_vf_loss = 0.5 * reduce_mean_valid(torch.pow(delta, 2.0))
+
+            # The entropy loss.
+            mean_entropy = reduce_mean_valid(_make_time_major(action_dist.entropy()))
+
+        else:
+            logger.debug("Using PPO surrogate loss (vtrace=False)")
+
+            # Prepare KL for Loss
+            _make_time_major(prev_action_dist.kl(action_dist))
+            reverse_action_kl = _make_time_major(action_dist.kl(prev_action_dist))
+            actions_logp = _make_time_major(action_dist.logp(actions))
+            prev_actions_logp = _make_time_major(prev_action_dist.logp(actions))
+            logp_ratio = torch.exp(actions_logp - prev_actions_logp)
+
+            advantages = _make_time_major(train_batch[Postprocessing.ADVANTAGES])
+            surrogate_loss = torch.min(
+                advantages * logp_ratio,
+                advantages
+                * torch.clamp(
+                    logp_ratio,
+                    1 - self.config["clip_param"],
+                    1 + self.config["clip_param"],
+                ),
+            )
+
+            # mean_kl_loss = reduce_mean_valid(action_kl)
+            mean_kl_loss = mean_reverse_kl_loss = reduce_mean_valid(reverse_action_kl)
+            mean_policy_loss = -reduce_mean_valid(surrogate_loss)
+
+            # The value function loss.
+            value_targets = _make_time_major(train_batch[Postprocessing.VALUE_TARGETS])
+            delta = values_time_major - value_targets
+            mean_vf_loss = 0.5 * reduce_mean_valid(torch.pow(delta, 2.0))
+
+            # The entropy loss.
+            mean_entropy = reduce_mean_valid(_make_time_major(action_dist.entropy()))
+
+        # The summed weighted loss.
+        total_loss = mean_policy_loss - mean_entropy * self.entropy_coeff
+        # Optional additional KL Loss
+        if self.config["use_kl_loss"]:
+            total_loss += self.kl_coeff * mean_reverse_kl_loss
+
+        # Optional vf loss (or in a separate term due to separate
+        # optimizers/networks).
+        loss_wo_vf = total_loss
+        if not self.config["_separate_vf_optimizer"]:
+            total_loss += mean_vf_loss * self.config["vf_loss_coeff"]
+
+        # Store values for stats function in model (tower), such that for
+        # multi-GPU, we do not override them during the parallel loss phase.
+        model.tower_stats["total_loss"] = total_loss
+        model.tower_stats["mean_policy_loss"] = mean_policy_loss
+        model.tower_stats["mean_kl_loss"] = mean_kl_loss
+        model.tower_stats["mean_reverse_kl_loss"] = mean_reverse_kl_loss
+        model.tower_stats["mean_vf_loss"] = mean_vf_loss
+        model.tower_stats["mean_entropy"] = mean_entropy
+        model.tower_stats["value_targets"] = value_targets
+        model.tower_stats["vf_explained_var"] = explained_variance(
+            torch.reshape(value_targets, [-1]),
+            torch.reshape(values_time_major, [-1]),
+        )
+
+        # Return one total loss or two losses: vf vs rest (policy + kl).
+        if self.config["_separate_vf_optimizer"]:
+            return loss_wo_vf, mean_vf_loss
+        else:
+            return total_loss
+
+    @override(APPOTorchPolicy)
+    def stats_fn(self, train_batch: SampleBatch) -> Dict[str, TensorType]:
+        stats = super().stats_fn(train_batch)
+        # stats.update({
+        #     "mmd_loss": torch.mean(torch.stack(self.get_tower_stats("mmd_loss"))),
+        #     "kl_div": torch.mean(torch.stack(self.get_tower_stats("kl_div"))),
+        #     "temperature": self.temp(self.iteration),
+        #     "mag_lr": self.mag_lr(self.iteration),
+        # })
+        return stats
+
+    @property
+    def comparator(self):
+        return self._comparator
+
+class MMDAPPO(APPO):
+    def get_default_policy_class(cls, config):
+        return MMDAPPOTorchPolicy
